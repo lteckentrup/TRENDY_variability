8fbd6e2cb54da97bf3e65e38e7158dbb4f1adced
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2020-04-30 09:23:39 +1000

    Update calculate_anomaly.sh

--- calculate_anomaly.sh
+++ calculate_anomaly.sh
@@ -4,7 +4,7 @@ for model in CLASS-CTEM CLM5.0 ISAM JSBACH JULES LPJ-GUESS LPJ LPX SURFEX; do
         $model'_'S2_gpp_climatology.nc
     cdo selyear,1901/2017 ../../S2_new/gpp/$model'_'S2_gpp.nc \
         $model'_'S2_gpp_1901-2017.nc
-    
+
     cdo -b F64 sub $model'_'S2_gpp_1901-2017.nc $model'_'S2_gpp_climatology.nc  \
         $model'_'S2_gpp_anomaly_trend.nc
     cdo -b F64 detrend $model'_'S2_gpp_anomaly_trend.nc \
@@ -26,13 +26,13 @@ for model in CABLE-POP DLEM OCN ORCHIDEE-CNP ORCHIDEE VISIT; do
     rm $model'_'S2_gpp_anomaly_trend.nc
 done
 
-for model in CABLE-POP CLASS-CTEM CLM5.0 JSBACH OCN ORCHIDEE-CNP ORCHIDEE SURFEX; do
+for model in CABLE-POP CLM5.0 OCN ORCHIDEE-CNP ORCHIDEE SURFEX; do
     mv $model'_'S2_gpp_anomaly.nc $model'_'S2_gpp_anomaly_original.nc
     cdo -b F64 remapycon,../fine_grid.txt $model'_'S2_gpp_anomaly_original.nc \
         $model'_'S2_gpp_anomaly.nc
 done
 
-for model in CABLE-POP CLASS-CTEM CLM5.0 DLEM ISAM JSBACH LPJ LPJ-GUESS LPX OCN ORCHIDEE-CNP ORCHIDEE SURFEX VISIT; do
+for model in CABLE-POP CLM5.0 DLEM ISAM LPJ LPJ-GUESS LPX OCN ORCHIDEE-CNP ORCHIDEE SURFEX VISIT; do
         cdo -b F64 -L -divc,1e+12 -yearsum -settaxis,1901-01-01,00:00,1month \
         -seldate,1901-07-16,2017-06-16 \
         -sellonlatbox,112.25,153.75,-43.75,-10.25 -mulc,86400 -muldpm \
@@ -40,3 +40,24 @@ for model in CABLE-POP CLASS-CTEM CLM5.0 DLEM ISAM JSBACH LPJ LPJ-GUESS LPX OCN 
         $model'_'S2_gpp_anomaly.nc \
         sh_year/$model'_'S2_gpp_anomaly_area_weighted.nc
 done
+
+module unload cdo
+module load cdo/1.6.1
+
+for model in CLASS-CTEM JSBACH; do
+    mv $model'_'S2_gpp_anomaly.nc $model'_'S2_gpp_anomaly_original.nc
+    cdo -b F64 remapcon,../fine_grid.txt $model'_'S2_gpp_anomaly_original.nc \
+        $model'_'S2_gpp_anomaly.nc
+done
+
+for model in CLASS-CTEM JSBACH; do
+        cdo -b F64 -L -divc,1e+12 -yearsum -settaxis,1901-01-01,00:00,1month \
+        -seldate,1901-07-16,2017-06-16 \
+        -sellonlatbox,112.25,153.75,-43.75,-10.25 -mulc,86400 -muldpm \
+        -mul $model'_'S2_gpp_anomaly.nc -gridarea \
+        $model'_'S2_gpp_anomaly.nc \
+        sh_year/$model'_'S2_gpp_anomaly_area_weighted.nc
+done
+
+module unload cdo
+module load cdo
