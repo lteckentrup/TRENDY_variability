996d8ebf000b3f83a45dd1e284209d838d9af4f1
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2020-04-30 13:17:54 +1000

    Update download_trendy.py

--- download_trendy.py
+++ download_trendy.py
@@ -1,22 +1,16 @@
-import pysftp as sftp
-
-pathwayLOCAL = '/srv/ccrc/data02/z5227845/research/TRENDY/'
-def sftpgrab(var, exp, model, suffix):
-    s = sftp.Connection(server, username=xxx, 
-                        password=yyy)
-   
     if model == 'CLASS-CTEM':
-        remotepath='/output/'+model+'/'+exp+'/'+model+'_'+exp+'_monthly_'+var+suffix    
-        localpath=pathwayLOCAL+exp+'/'+var+'/'+model+'_'+exp+'_'+var+suffix 
+        if var == 'lai':
+            remotepath='/output/'+model+'/'+exp+'/'+model+'_'+exp+'_monthly_perpft_'+var+suffix    
+            localpath=pathwayLOCAL+exp+'/'+var+'/'+model+'_'+exp+'_'+var+suffix 
+        else:
+            remotepath='/output/'+model+'/'+exp+'/'+model+'_'+exp+'_monthly_'+var+suffix    
+            localpath=pathwayLOCAL+exp+'/'+var+'/'+model+'_'+exp+'_'+var+suffix 
     elif model == 'LPX':
         remotepath='/output/'+model+'-Bern/'+exp+'/'+model+'_'+exp+'_monthly_'+var+suffix    
         localpath=pathwayLOCAL+exp+'/'+var+'/'+model+'_'+exp+'_'+var+suffix 
     elif var == 'lai':
         if model in ('JSBACH', 'LPJ-GUESS'):
             remotepath='/output/'+model+'/'+exp+'/'+model+'_'+exp+'_'+var+'pft'+suffix    
-            localpath=pathwayLOCAL+exp+'/'+var+'/'+model+'_'+exp+'_'+var+suffix 
-        elif model == 'CLASS-CTEM':
-            remotepath='/output/'+model+'/'+exp+'/'+model+'_'+exp+'_monthly_perpft_'+var+suffix    
             localpath=pathwayLOCAL+exp+'/'+var+'/'+model+'_'+exp+'_'+var+suffix 
         else:
             remotepath='/output/'+model+'/'+exp+'/'+model+'_'+exp+'_'+var+suffix    
@@ -43,4 +37,4 @@ suffices=['.nc.gz', '.nc' , '.nc', '.nc', '.nc.gz', '.nc', '.nc.bz2', '.nc',
           '.nc', '.nc', '.nc', '.nc', '.nc', '.nc.gz', '.nc', '.nc.gz']
 
 for m, s in zip(modelz, suffices):
-    sftpgrab('lai', 'S2', m, s)
+    sftpgrab('mrso', 'S2', m, s)
